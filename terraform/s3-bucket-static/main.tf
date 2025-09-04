863ab5b6e7d8b2f0bd8c78afaf91f79fe5d43e62
rafaelserpaa <rafaelserpa01@gmail.com>
2025-09-04 01:07:08 -0300

    style: format Terraform configuration for consistency

--- terraform/s3-bucket-static/main.tf
+++ terraform/s3-bucket-static/main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
     region = "us-east-1"
 }
+
 variable "bucket_name" {
-    type        = string
+    type = string
 }
 
 resource "aws_s3_bucket" "static_site_bucket" {
@@ -11,11 +12,12 @@ resource "aws_s3_bucket" "static_site_bucket" {
     website {
         index_document = "index.html"
         error_document = "404.html"
-    }   
+    }
+
     tags = {
-        Name        = "Static Site Bucket"
+        Name = "Static Site Bucket"
         Environment = "Production"
-    }   
+    }
 }
 
 resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
@@ -28,17 +30,18 @@ resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
-    bucket = aws_s3_bucket.static_site_bucket.id
-
-    rule {
-        object_ownership = "BucketOwnerPreferred"
-    }
+  bucket = aws_s3_bucket.static_site_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
 
 resource "aws_s3_bucket_acl" "static_site_bucket" {
-    depends_on = [
-        aws_s3_bucket_public_access_block.static_site_bucket,
-        aws_s3_bucket_ownership_controls.static_site_bucket
-    ]
-    acl   = "public-read"
+  depends_on = [
+	aws_s3_bucket_public_access_block.static_site_bucket,
+	aws_s3_bucket_ownership_controls.static_site_bucket,
+  ]
+
+  bucket = aws_s3_bucket.static_site_bucket.id
+  acl    = "public-read"
 }
